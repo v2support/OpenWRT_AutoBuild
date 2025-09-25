c1946401dbfb52a51c55339ef173791e08af57e0
v2support <132957347+v2support@users.noreply.github.com>
2025-09-25 10:40:40 -0400

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#echo "执行自定义脚本第一部分 - 设置系统参数"
+#
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (在更新前执行)
+#
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#
+#echo "系统参数设置完成"
+#
